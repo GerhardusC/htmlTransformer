cf25a88d82fcb2a2c618fe09ba003420b0399086
Gerhardus <hardus.cronje@gmail.com>
2025-09-12 23:29:54 +0200

    chore: removed comments

--- src/server.rs
+++ src/server.rs
@@ -3,11 +3,9 @@ use axum::{routing::post, Router};
 use crate::{errors::ApplicationError, handlers::transform_case_handler};
 
 pub async fn create_server(addr: &str) -> Result<(), ApplicationError> {
-    // build our application with a single route
     let app = Router::new()
         .route("/transform", post(transform_case_handler));
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, app).await?;
     Ok(())
