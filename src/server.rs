16b6c017148bfd9b07ba6d9163d579751514ecc8
Gerhardus <hardus.cronje@gmail.com>
2025-09-15 22:52:02 +0200

    chore: merge conflict

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,7 @@
-use axum::{
-    Router,
-    routing::{get, post},
-};
+use axum::{routing::{get, get_service, post}, Router};
 
-use crate::{
-    errors::ApplicationError,
-    handlers::{help_page, transform_case_handler},
-};
+use crate::{errors::ApplicationError, handlers::{help_page, transform_case_handler}};
+use tower_http::services::ServeDir;
 
 pub async fn create_server(addr: &str) -> Result<(), ApplicationError> {
     let app = Router::new()
@@ -16,4 +11,14 @@ pub async fn create_server(addr: &str) -> Result<(), ApplicationError> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, app).await?;
     Ok(())
+}
+
+fn routes() -> Router {
+    let app = Router::new()
+        .route("/transform", post(transform_case_handler));
+    app
+}
+
+fn routes_static(serve_dir: &str) -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new(serve_dir)))
 }
